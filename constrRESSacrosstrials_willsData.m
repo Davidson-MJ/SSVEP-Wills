b9fd73f8b77fddee609c073f30e7cc7adf2a4734
Matt Davidson <mjd070@gmail.com>
2018-09-04 18:37:49 +1000

    novel scripts committed

---
+++ constrRESSacrosstrials_willsData.m
@@ -0,0 +1,212 @@
+function [ressEVEC, ressTS, ressSNR, ressMAPS, hz]= constrRESSacrosstrials_willsData(dataIN, peakfreqsare, usechans, neighbour, window,srate)
+
+
+peakwidt  = .5; % FWHM at peak frequency for stimulus
+
+%%%% these need to be calibrated for our closely spaced freqs.
+
+
+    peakfreqsall=[5,15,20,30, 35, 40,45, 60];
+
+% this bit hard coded, 6 s window length
+% epochdur = sum(abs([-3 3]));
+% timeid = [0:1/srate:epochdur];
+% timeid= timeid-3;
+% 
+% 
+% tidx= dsearchn(timeid', [window]');
+
+tidx=[1 size(dataIN,2)] ; %use  all timepoints..
+        
+
+%% remove any trials woth nan (skips trials in RESS)
+badtrials = find(isnan(dataIN(1,[tidx(1)],:)));
+tid=[1:size(dataIN,3)];
+tid(badtrials)=[];
+
+%we won't use that trial(s) for covariance matrix construction.
+% data = data(:,:,tid);
+
+
+% extract EEG data for RESS
+dataIN = squeeze(dataIN(usechans,:,tid));
+
+useneighs=0;
+
+
+peakfreq1=peakfreqsare(1);
+       
+        
+        ndet = find(peakfreqsall==peakfreq1);
+        try            
+neighfreqLo = neighbour(ndet).centrefreqs(1);  % distance of neighboring frequencies away from peak frequency, +/- in Hz
+neighwidtLo = neighbour(ndet).sd(1);  % FWHM of the neighboring frequencies
+
+neighfreqHi = neighbour(ndet).centrefreqs(2);  % distance of neighboring frequencies away from peak frequency, +/- in Hz
+neighwidtHi = neighbour(ndet).sd(2);  % FWHM of the neighboring frequencies
+%recommended by Cohen & Gulbinaite, Neuroimage, 2017.
+        catch % in case 20Hz          
+        end
+        
+        % FFT parameters
+        nfft = ceil( srate/.1 ); % .1 Hz resolution
+        hz    = linspace(0,srate,nfft);
+
+        %% RESS begin
+
+        
+        % compute covariance matrix at peak frequency
+        
+
+        %filter at interest freq
+            fdatAt = filterFGx(dataIN,srate,peakfreq1,peakwidt,0);
+        
+        
+        fdatAt = reshape( fdatAt(:,tidx(1):tidx(2),:), length(usechans),[] );
+        fdatAt = bsxfun(@minus,fdatAt,mean(fdatAt,2)); %subtract so mean =0, v.important.
+        covAt  = (fdatAt*fdatAt')/diff(tidx); %make sure chans by chans
+        
+        % are we using neighbourhood for reference, or broadband?
+        if useneighs==1
+            % compute covariance matrix for lower neighbor
+            
+            try fdatLo = filterFGx(dataIN(:,:,itrial),srate,neighfreqLo,neighwidtLo);
+            catch
+                fdatLo = filterFGx(dataIN,srate,neighfreqLo,neighwidtLo,0);
+            end
+            
+            fdatLo = reshape( fdatLo(:,tidx(1):tidx(2),:), length(usechans),[] );
+            fdatLo = bsxfun(@minus,fdatLo,mean(fdatLo,2));
+            covLo  = (fdatLo*fdatLo')/diff(tidx);
+            
+            % compute covariance matrix for upper neighbor
+            try fdatHi = filterFGx(dataIN(:,:,itrial),srate,neighfreqHi,neighwidtHi,0);
+            catch
+                fdatHi = filterFGx(dataIN,srate,neighfreqHi,neighwidtHi,0);
+            end
+            
+            fdatHi = reshape( fdatHi(:,tidx(1):tidx(2),:), length(usechans),[] );
+            fdatHi = bsxfun(@minus,fdatHi,mean(fdatHi,2));
+            covHi  = (fdatHi*fdatHi')/diff(tidx);
+            
+            %%
+            [evecs,evals] = eig(covAt,(covHi+covLo)/2); %finds eig vector of sig vs noise.
+            
+        else %use broadband signal
+            %               fdatBB=filterFGx(dataIN,srate,20,20,1);
+            fdatBB = reshape( dataIN(:,tidx(1):tidx(2),:), length(usechans),[] );
+            fdatBB = bsxfun(@minus,fdatBB,mean(fdatBB,2));
+            covBB  = (fdatBB*fdatBB')/diff(tidx);
+            
+            [evecs,evals] = eig(covAt,(covBB)); %finds eig vector of sig vs noise.
+        end
+        
+        %%
+        [~,comp2plot] = max(diag(evals)); % find maximum component
+        evecs = bsxfun(@rdivide,evecs,sqrt(sum(evecs.^2,1)));
+        % normalize vectors (not really necessary, but OK) - good if diff length
+        % epochs/cross ppants.
+        
+        % extract components and force sign
+        % maps = inv(evecs'); % get maps (this is fine for full-rank matrices)
+        maps = covAt * evecs / (evecs' * covAt * evecs); % this works either way
+        %
+        [~,idx] = max(abs(maps(:,comp2plot))); % find biggest component in map
+        maps = maps * sign(maps(idx,comp2plot)); % force to positive sign
+        
+        %%
+%         % reconstruct RESS component time series, for this trial.
+%         ress_ts1 = [];
+%         
+%        % reconstruct RESS component time series
+        ress_ts1 = zeros(size(dataIN,2),size(dataIN,3));
+        for ti=1:size(dataIN,3)
+            ress_ts1(:,ti) = evecs(:,comp2plot)'*squeeze(dataIN(:,:,ti));
+        end
+      ressTS=ress_ts1;
+        %
+        ressEVEC=evecs(:,comp2plot);
+        
+%         
+        ressxAll = (abs( fft(ress_ts1(tidx(1):tidx(2),:),nfft,1)/diff(tidx) ).^2);
+        ressx= nanmean(ressxAll,2);
+%         
+%         
+%         
+%         % compute SNR for this trial:
+%         [snrR,snrE] = deal(zeros(size(hz)));
+%         
+%         snrRall=zeros(size(ressxAll));
+%         
+%         %%% very important, compare again to the correct frequencies 
+%         %we have used
+%         %low shoulder
+% %         
+%         skipbinsLo =  dsearchn(hz', [peakfreq1-neighbour(ndet).snrlow(2)]'); % hard-coded in Hz, how many away before start shoulder.        
+%         numbinsLo  = dsearchn(hz', [peakfreq1-neighbour(ndet).snrlow(1)]'); % hard-coded in Hz, how many away before start shoulder.        
+%         
+%         
+%         skipbinsHi =  dsearchn(hz', [neighbour(ndet).snrhigh(1)- peakfreq1]'); % hard-coded in Hz, how many away before start shoulder.        
+%         numbinsHi  = dsearchn(hz', [neighbour(ndet).snrhigh(2) - peakfreq1]'); % hard-coded in Hz, how many away before start shoulder.        
+% 
+
+snrR=zeros(1,length(ressx));
+% 
+% for hzi=numbinsLo+1:length(hz)-numbinsHi-1
+%     numer = ressx(hzi);
+%     denom = mean( ressx([hzi-numbinsLo:hzi-skipbinsLo hzi+skipbinsHi:hzi+numbinsHi]) );
+%     snrR(hzi) = numer./denom;
+%     
+% end
+
+
+%% alternate method for SNR.
+% skipbins =  6; % *.1 - make sure it exceeds the Hbw (0.5Hz for 2s window)
+% numbins  = 20+skipbins; %  2 Hz, also hard-coded!
+% for hzi=numbins+1:length(hz)-numbins-1
+%     numer = ressx(hzi);
+%     denom = mean( ressx([hzi-numbins:hzi-skipbins hzi+skipbins:hzi+numbins]) );
+%     snrR(hzi) = numer./denom;
+%     
+% end
+
+% plot(hz, snrR)
+% % % hold on
+% xlim([ 0 25])
+% % % % %%
+% plot(hz, 10*log10(ressx.^2))
+% hold on
+% % plot(hz, snrR2)
+% xlim([0 25])
+% shg
+%%
+% end
+ressSNR=snrR;
+        
+        % store map
+        % also topo for inspection
+        map2plot = maps(:,comp2plot);
+        map2save=map2plot./max(map2plot);
+        
+        if ~isreal(map2save)
+            error(' ')
+        end
+        
+        %save original trial by trial data for comparison.
+        try ressTS(itrial,ifreq,:) = ress_ts1';
+            ressSNR(itrial,ifreq,:) = snrR;
+            
+            ressMAPS(itrial,ifreq,:) = map2save;
+        catch
+%             ressTS = ress_ts1';
+%             ressSNR = snrR;
+            
+            ressMAPS = map2save;
+            
+        end
+        
+        
+    
+
+
+end
---
+++ s3_EEG_E_createRESSfiltersperppant.m
@@ -0,0 +1,275 @@
+% Motivation is to create unique RESS spatial filters per participant and
+% frequency combo.
+
+
+%we need all the trials in which the targets were present,
+
+
+clear all
+
+%% UPDATED for Wills data:
+
+try cd('/Users/MattDavidson/Desktop/SSVEP-feedbackproject/AA_ANALYZED DATA')
+%     addpath(
+catch
+end
+basefol=pwd;
+
+clearvars -except basefol allppants
+dbstop if error
+
+
+job.concatLiketrialsperppant=1; %also performs RESS, we are left with 16 types of spatial filter, per ppant. 
+
+% job.applyRESSpertrialtype=1; %after constructing RESS filters in the previous step, here we apply to all relevant trials, 
+% %also reduces the size of the 
+
+%set up directories and dependencies.
+getelocs;
+
+cd([basefol filesep 'EEG'])
+eegfol=pwd;
+
+pdirs = dir([pwd filesep '*_*' 'EEG']);
+
+%% load data to determine physical catch timing.
+
+
+% allppants=1:19;
+
+%remaining participants after behavioral data analysis/exclusion
+allppants=[1,2,4,6,9:16,18]; %
+
+window=[-3 3];
+
+srate=250;
+
+epochdur = sum(abs(window))*srate;
+
+timeid = [0:1/srate:epochdur];
+timeid= timeid-3;
+
+onsetc = ceil(epochdur)/2;
+% peakfreqsare=[20,40]; %hz
+%timing
+
+%%
+if job.concatLiketrialsperppant==1
+    
+    
+    
+    % SET UP RESS params.
+
+    peakfreqsare=[5,15,20,30, 35, 40,45, 60];
+    
+    neighbour=[];
+    for ihz = 1:length(peakfreqsare)
+    
+        hzAT = peakfreqsare(ihz);
+        
+        %needs to be narrow to avoid other stim freqs.  
+    neighbour(ihz).centrefreqs=[hzAT-1 , hzAT+1];   % a low and high centre freq for covR
+    neighbour(ihz).sd=[.5, .5]; %FWHM of those...
+    neighbour(ihz).snrlow = [hzAT-1.5 hzAT-.5];    %not so important, were being used in SNR calcs. now in a laterscript
+    neighbour(ihz).snrhigh = [hzAT+.5 hzAT+1.5];
+    
+    
+    end
+
+    %%
+    windowsmall=[];
+    windowsmall(1,:) = [-3 -1] ;%  window targets present
+    windowsmall(2,:) = [1 3] ;%  window targets present after BP
+    %%
+%     usechans=[51:64, 17:32]; %occipital
+    usechans=[1:64]; %occipital
+    
+    
+    for ifol =allppants
+        
+        
+        %%
+        cd(eegfol)
+        cd(pdirs(ifol).name)
+        
+        
+        %% load the relevant PFI data.
+        load('ppant_PFI_Epoched');
+        load('ppant_Catch_Epoched')
+        
+        
+        %%
+        
+        ressEVEC_byHz = zeros(length(peakfreqsare), length(usechans));
+        ressEVEC_byHz_MAPS=zeros(length(peakfreqsare),length(usechans));
+        
+        for ifreq=1:length(peakfreqsare)
+            
+            
+                % collect relevant trials for each type of spatial
+                % configuration/filter construction.
+              
+                %%
+                for id=1:10% Use all epochs so as not to bias condition comparisons.
+                    
+                    switch id
+                        case 1
+                            dataIN=ppant_SNREEG_PFI_0_1;
+%                             durscheck=durs0_1;
+                            usewindow=1:2; 
+                        case 2
+                            dataIN=ppant_SNREEG_PFI_1_0;
+                            
+%                             durscheck=durs1_0;
+                            usewindow=1:2; 
+                        case 3
+                            dataIN=ppant_SNREEG_PFI_1_2;
+%                             searchtrials = [Freqwas.dir1_2.Trialind];
+%                             durscheck=durs1_2;
+                            usewindow=1:2; 
+                        case 4
+                            dataIN=ppant_SNREEG_PFI_2_1;
+%                             searchtrials = [Freqwas.dir2_1.Trialind];
+%                             durscheck=durs2_1;
+                            usewindow=1:2; 
+                        case 5
+                            dataIN=ppant_SNREEG_PFI_3_2;
+%                             searchtrials = [Freqwas.dir3_2.Trialind];
+%                             durscheck=durs3_2;
+                            usewindow=1:2;
+                        case 6
+                            dataIN=ppant_SNREEG_PFI_2_3;                            
+%                             searchtrials = [Freqwas.dir2_3.Trialind];
+%                             durscheck=durs2_3;
+                            usewindow=1:2;
+                        case 7
+                            dataIN=ppant_SNREEG_disapBPwithincatch;
+%                             searchtrials = [1:24]; 
+                            usewindow=1; 
+                        case 8
+                            dataIN=ppant_SNREEG_reapBPaftercatch;
+%                             searchtrials = [1:24]; %which catch was disappearing or not?
+                            usewindow=2; 
+                        case 9
+                            dataIN=ppant_SNREEG_catchramponset;
+%                             searchtrials = [1:24]; %which catch was disappearing or not?
+                            usewindow=1;
+                        case 10
+                            dataIN=ppant_SNREEG_catchrampoffset;
+%                             searchtrials = [1:24]; %which catch was disappearing or not?
+                            usewindow=2;
+                        
+                        
+                            
+                    end
+                    
+                    trialtypesperTargPresent= 1:size(dataIN,1);
+                    
+                    % now that we have a datatype, get the right epochs that share stimulus configuration
+                    
+                    % also correct window (pre or post indication of target
+                    % presence)
+                    for windch=1:length(usewindow)
+                        wind=usewindow(windch);
+                    windcheck= windowsmall(wind,:);
+                    
+                    tidx=dsearchn(timeid', [windcheck]');
+                    
+                    
+                    %reduce size.
+                    datast= squeeze(dataIN(trialtypesperTargPresent,:,tidx(1):tidx(2)));
+                    
+                    durscheck=[];
+                    %% check for bad trials (noisy)
+                    %std per trial(average over electrodes)
+                    tmp=[];
+                    if ndims(datast)<3
+                        tmp(1,:,:)= datast;
+                        datast=tmp;
+                    end
+                    datastSD = nanstd(squeeze(nanmean(datast,2)),0,2);
+                    datastSDchan = nanmean(nanstd(datast,0 ,3),1);
+                    %remove those with 2.5*std from mean.
+                    trialSD=nanstd(datastSD);
+                    mSD=nanmean(datastSD);
+                    keeptrials=1:size(datast,1);
+                    
+                    %remove the trials with excessive noise.
+%                     badtrials=find(datastSD>mSD+2.5*trialSD)';
+%                     badtrials=find(datastSD>mSD)';
+                    badtrials = find(datastSD>100)';
+                    % also skip the trials which were only transient button
+                    % presses. (less than one second).                    
+                    shorttrials = find(durscheck<60);
+                    badtrials = [badtrials, shorttrials];
+                    
+                    % remove these from consideration.
+                    keeptrials(badtrials)=[];
+                    datast=datast(keeptrials,:,:);
+                    
+                    %sanity check this is clean data:
+                    figure(1); subplot(211); plot(squeeze(datast(:,62,:))');
+                    subplot(212); topoplot(datastSDchan, elocs(1:64)); c=colorbar; ylabel(c, 'SD');
+                    %shows how noisy the channels are!
+                    
+                    
+                    % concatenate all these types to increase quality of cov matrix.
+                    if id==1 && windch==1%start here
+                        if ndims(datast)<3
+                        dataOUT(1,:,:)=datast;
+                        else
+                            dataOUT=datast;
+                        end
+                        
+                    else
+                        % append
+                         if ndims(datast)<3
+                        tmp(1,:,:)=datast;
+                        dataOUT=cat(1,dataOUT,tmp);
+                        
+                        else
+                            dataOUT=cat(1,dataOUT,datast);
+                         end                        
+                        
+                    end
+                    
+                    end
+                    
+                end
+                
+                % We want a single component for this type of config, per ppant.
+                % trials as last dimension
+                %%
+                ressd=permute(dataOUT,[2 3 1]);
+                peakfreq1= peakfreqsare(ifreq);
+                
+                
+                
+                [ressEVEC, ~, ~, ressMAPS, hz]= constrRESSacrosstrials_willsData(ressd, peakfreq1, usechans, neighbour, window,srate);
+                
+                %
+                
+                % now we can multiple each type of data by the appropriate
+                %                 % ressEVEC
+    
+                figure(ifol); subplot(2,4,ifreq)
+                
+                topoplot(ressMAPS, elocs(usechans));
+title(num2str(peakfreq1));
+                %%
+                ressEVEC_byHz(ifreq,:) = ressEVEC;
+                ressEVEC_byHz_MAPS(ifreq,:)= ressMAPS;
+            
+            
+            
+        end
+        
+            savename='RESSfilterdata';
+        
+        save(savename, 'ressEVEC_byHz', 'ressEVEC_byHz_MAPS', 'usechans', 'neighbour', 'windowsmall')
+        disp(['fin for ppant' num2str(ifol)])
+    end
+end
+
+
+
